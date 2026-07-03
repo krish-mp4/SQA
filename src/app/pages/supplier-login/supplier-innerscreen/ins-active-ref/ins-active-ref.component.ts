17d3b3a0b44306c84cd1591aefff013cdd85f6bb
lokeshkarakatla <lokeshkarakatla4@gmail.com>
2026-07-03 12:30:13 +0530

    Created new components for Supplier->InspectionReferenece(Inner Screen) and added routing for them

---
+++ src/app/pages/supplier-login/supplier-innerscreen/ins-active-ref/ins-active-ref.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ins-active-ref',
+  templateUrl: './ins-active-ref.component.html',
+  styleUrls: ['./ins-active-ref.component.scss']
+})
+export class InsActiveRefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
